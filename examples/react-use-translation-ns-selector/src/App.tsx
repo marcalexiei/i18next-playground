90b17b0bfdad1a4a3354764afb16647aa68e504d
Andrew Jarrett <ahrjarrett@gmail.com>
2025-08-08 05:37:12 -0500

    add an example with 2 namespaces selected

--- examples/react-use-translation-ns-selector/src/App.tsx
+++ examples/react-use-translation-ns-selector/src/App.tsx
@@ -1,8 +1,45 @@
 import { t } from 'i18next'
 import { useTranslation } from "react-i18next"
 
-export function App() {
+export function Ns1() {
   const { t } = useTranslation(["ns1"])
+
+  return (
+    <>
+      {t(($) => $.job_details.title, { ns: 'ns1' })}
+
+      {/**
+       * This should error since `invalidns` do not exists
+       */}
+      {t(($) => $.job, { ns: "invalidns" })}
+
+      {/* 
+        There 2 possible missing errors here:
+        - `ns2` should not be a valid `ns` 
+          since is not included in useTranslation
+        - job key doesn't exist on `ns2`
+      */}
+      {t(($) => $.job, { ns: "n2" })}
+
+      {/*
+        Ignoring the fact that `ns2` should not be used here,
+        the types provided in `$` for `ns2` are incorrect.
+        description.part1` is available in n2 
+      */}
+      {t(($) => $.description.part1, { ns: "n2" })}
+
+      {t($ => $.job)}
+      {t($ => $.job, { ns: 'ns1' })}
+      {t($ => $.job, { ns: '' })}
+      {t($ => $.job, { ns: 'ns2' })}
+      {t($ => $.description.part1, { ns: 'ns2' })}
+      {t($ => $.description.part1, { ns: '' })}
+    </>
+  )
+}
+
+export function Ns1And2() {
+  const { t } = useTranslation(["ns1", "ns2"])
 
   return (
     <>
