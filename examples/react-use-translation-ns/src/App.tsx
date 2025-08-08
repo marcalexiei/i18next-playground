7623b964bfd7337e3ba9433ec8fbbe9098574c02
Andrew Jarrett <ahrjarrett@gmail.com>
2025-08-07 19:19:17 -0500

    adds example + comment explaining my expectations about useTranslation's behavior

--- examples/react-use-translation-ns/src/App.tsx
+++ examples/react-use-translation-ns/src/App.tsx
@@ -1,23 +1,53 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next"
 
 export function App() {
-  const { t } = useTranslation(["ns1"]);
+  const { t } = useTranslation(["ns1"])
 
   return (
     <>
       {t("ns1:job")}
-      {t("ns2:test")}
+      {
+        /**
+         * This TypeError makes sense to me. I don't think this should be possible with 
+         * the selector API either (correct me if I'm wrong though)
+         */
+      }
+      {t("ns2:description.part1")}
+      {
+        /**
+         * I thought this (below) is what you were saying should raise a TypeError,
+         * since `useTranslation` only added `ns1` when it was instantiated
+         * 
+         * https://github.com/i18next/react-i18next/pull/1852#discussion_r2256990517
+         */
+      }
+      {t("description.part1", { ns: 'ns2' })}
     </>
-  );
+  )
 }
 
 export function App2() {
-  const { t } = useTranslation(["ns2"]);
+  const { t } = useTranslation(["ns2"])
 
   return (
     <>
+      {
+        /**
+         * This TypeError makes sense to me. I don't think this should be possible with 
+         * the selector API either (correct me if I'm wrong though)
+         */
+      }
       {t("ns1:job")}
       {t("ns2:description.part1")}
+      {
+        /**
+         * I thought this (below) is what you were saying should raise a TypeError,
+         * since `useTranslation` only added `ns2` when it was instantiated
+         * 
+         * https://github.com/i18next/react-i18next/pull/1852#discussion_r2256990517
+         */
+      }
+      {t("job", { ns: 'ns1' })}
     </>
-  );
+  )
 }
