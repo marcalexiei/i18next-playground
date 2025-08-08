b32149c534e7d742a7eced339aff29e7be062c40
Marco Pasqualetti <marco.pasqualetti@live.com>
2025-08-08 07:31:40 +0200

    add react-use-translation-ns-selector

---
+++ examples/react-use-translation-ns-selector/src/i18next.d.ts
@@ -0,0 +1,9 @@
+import type { defaultNS, resources } from "./index.js";
+
+declare module "i18next" {
+  interface CustomTypeOptions {
+    defaultNS: typeof defaultNS;
+    resources: (typeof resources)["en"];
+    enableSelector: true;
+  }
+}
