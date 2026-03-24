6497452bc082452bb9d12b5721687b01d4d7de10
Marco Pasqualetti <marco.pasqualetti@live.com>
2026-03-24 10:25:46 +0100

    feat: update i18next deps

--- examples/react-trans-component-type-safe-resources-const/src/App.tsx
+++ examples/react-trans-component-type-safe-resources-const/src/App.tsx
@@ -3,6 +3,7 @@ import { Trans } from "react-i18next";
 export function TranCmp() {
   return (
     <div>
+      {/* @ts-expect-error */}
       <Trans ns="custom_ns" i18nKey="hello" values={{ name: 1 }} />
     </div>
   );
